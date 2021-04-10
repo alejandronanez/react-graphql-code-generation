98dc88780374d6c4229c4b9a3dc0b839f0835c52
Alejandro Nanez <alejonanez@gmail.com>
2021-04-10 13:23:33 -0500

    All set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { ManualPikachu } from './ManualPikachu';
 
 function App() {
   return (
     <div>
       <h1>GraphQL Code Generation</h1>
+      <ManualPikachu />
     </div>
   );
 }
--- src/ManualPikachu.tsx
+++ src/ManualPikachu.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { gql } from '@apollo/client/core';
+import { Pokemon } from './Pokemon';
+import { useQuery } from '@apollo/client';
+
+const pikachuQuery = gql`
+  query pikachu {
+    pokemon(name: "pikachu") {
+      name
+      image
+    }
+  }
+`;
 
 export function ManualPikachu() {
-  return null;
+  const { data, loading } = useQuery(pikachuQuery);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <Pokemon name={data.pokemon.name} imageSrc={data.pokemon.image} />;
 }
